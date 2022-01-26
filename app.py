34063168d81c1376ed1902e7c76b83689afd7ce0
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-01-26 15:34:59 -0500

    Finished add

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-# Put your app in here.
+from flask import Flask, request
+import operations
+app = Flask(__name__)
+
+@app.get("/add")
+def add_num():
+    a = request.args["a"]
+    b = request.args["b"]
+    num = operations.add(int(a),int(b))
+    return f"<html><body>{num}</body></html>"
+
