4e6d110f560406201a918abb3e358e515db0cb82
Nate Snyder <n8.snyder@berkeley.edu>
2022-01-26 15:25:20 -0800

    finished app routes

--- app.py
+++ app.py
@@ -1,11 +1,45 @@
 from flask import Flask, request
 import operations
+
 app = Flask(__name__)
+
 
 @app.get("/add")
 def add_num():
+    """Page showing addition of query params a and b"""
     a = request.args["a"]
     b = request.args["b"]
-    num = operations.add(int(a),int(b))
+    num = operations.add(int(a), int(b))
     return f"<html><body>{num}</body></html>"
 
+
+@app.get("/sub")
+def sub_num():
+    """Page showing subtraction of query params a and b"""
+    a = request.args["a"]
+    b = request.args["b"]
+    num = operations.sub(int(a), int(b))
+    return f"<html><body>{num}</body></html>"
+
+
+@app.get("/mult")
+def mult_num():
+    """Page showing multiplication of query params a and b"""
+
+    a = request.args["a"]
+    b = request.args["b"]
+    num = operations.mult(int(a), int(b))
+    return f"<html><body>{num}</body></html>"
+
+
+@app.get("/div")
+def div_num():
+    """Page showing division of query params a and b"""
+
+    a = request.args["a"]
+    b = request.args["b"]
+    num = operations.div(int(a), int(b))
+    return f"<html><body>{num}</body></html>"
+
+
+# TODO: ADD DOCSTRINGS
